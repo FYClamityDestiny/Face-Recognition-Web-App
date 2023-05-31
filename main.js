de877d1ba983c1198c4890f91abe1566c82660ad
FYClamityDestiny <99613329+FYClamityDestiny@users.noreply.github.com>
2023-05-30 20:56:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -14,3 +14,20 @@ document.getElementById("image").innerHTML = "<img id='pic' src='"+data_uri+"'>"
 }
 console.log("Ml5 Version:",ml5.version);
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/GpbZWOwqh/model.json",modelLoaded);
+function modelLoaded(){
+console.log("Model is loaded.")
+}
+function identifyImage(){
+img = document.getElementById("pic")
+classifier.classify(img,gotResults);
+}
+function gotResults(){
+if(error){
+console.error();
+}
+else{
+console.log(results);
+document.getElementById("objectname").innerHTML = results[0].label;
+document.getElementById("objectaccuracy").innerHTML = results[0].confidence.toFixed(3);
+}
+}
